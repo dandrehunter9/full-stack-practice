abdef2e04be38f03d1ef6ef76fe6abf8c94650d8
dandrehunter9 <dandrehunter9@gmail.com>
2025-05-20 02:26:15 -0400

    successfully can create user and enter it into the database through swaggerui

--- db/models.py
+++ db/models.py
@@ -1,6 +1,10 @@
+from sqlalchemy.sql.sqltypes import Integer, String, Boolean
 from db.database import Base
-from sqlalchemy import column
+from sqlalchemy import Column
 
 class DBUser(Base):
     __tablename__ = 'users'
-    id = column
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String)
+    email = Column(String)
+    password = Column(String)
--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 from fastapi import FastAPI, status, Response
+from db.database import engine
 from enum import Enum
 from typing import Optional
 from router import blog_get
 from router import blog_post
+from router import user
+from db import models
+
 app = FastAPI()
 app.include_router(blog_get.router)
 app.include_router(blog_post.router)
+app.include_router(user.router)
 
 @app.get('/')
 def index():
     return {'message' : 'hello world'}
+
+models.Base.metadata.create_all(engine)
 
 
