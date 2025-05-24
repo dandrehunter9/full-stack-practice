edefab8326686cb908aa797ec107246ed56bcd96
dandrehunter9 <dandrehunter9@gmail.com>
2025-05-24 04:01:35 -0400

    added read, update, and delete functionality to user

--- db/db_user.py
+++ db/db_user.py
@@ -1,10 +1,10 @@
 from db.hash import Hash
 from sqlalchemy.orm.session import Session
 from schemas import UserBase
-from db.models import DBUser
+from db.models import DbUser
 
 def create_user(db: Session, request: UserBase):
-    new_user = DBUser(
+    new_user = DbUser(
         username = request.username,
         email = request.email,
         password = Hash.bcrypt(request.password)
@@ -13,3 +13,27 @@ def create_user(db: Session, request: UserBase):
     db.commit()
     db.refresh(new_user)
     return new_user
+
+def get_all_users(db: Session):
+    return db.query(DbUser).all()
+
+def get_user(db: Session, id: int):
+    return db.query(DbUser).filter(DbUser.id == id).first()
+
+def update_user(db: Session, id: int, request: UserBase):
+    user = db.query(DbUser).filter(DbUser.id == id)
+    # handle any exceptions
+    user.update({
+        DbUser.username: request.username,
+        DbUser.email: request.email,
+        DbUser.password: Hash.bcrypt(request.password)
+    })
+    db.commit()
+    return 'ok'
+
+def delete_user(db: Session, id: int):
+    user = db.query(DbUser).filter(DbUser.id == id)
+    # handle any exceptions
+    user.delete()
+    db.commit()
+    return 'user has been deleted'
--- schemas.py
+++ schemas.py
@@ -1,6 +1,21 @@
 from pydantic import BaseModel
+from typing import List
+
+#Article inside of the UserDisplay
+class Article(BaseModel):
+    title: str
+    content: str
+    published
 
 class UserBase(BaseModel):
     username: str
     email: str
     password: str
+
+
+class UserDisplay(BaseModel):
+    username: str
+    email: str
+    items: List[Article] = {}
+    class Config():
+        orm_mode = True
