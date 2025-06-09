e6883a736dba5541d8cb00329a0788bdd1fd7033
dandrehunter9 <dandrehunter9@gmail.com>
2025-06-09 00:49:16 -0400

    Added article table to database with a relationship to the user

--- router/article.py
+++ router/article.py
@@ -3,7 +3,7 @@ from schemas import ArticleBase, ArticleDisplay
 from fastapi import FastAPI, APIRouter, Depends
 from sqlalchemy.orm import Session
 from db.database import get_db
-from db import db_article
+from db import db_article 
 
 router = APIRouter(
     prefix='/article',
