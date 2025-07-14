2f37b0813d4b23f039b2034c74c69fe7105cda86
dandrehunter9 <dandrehunter9@gmail.com>
2025-07-14 03:11:14 -0400

    Added file.py to understand FastAPI file handling

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 from fastapi import FastAPI, status, Response, Request
 from fastapi.responses import JSONResponse, PlainTextResponse
+from sqlalchemy import true
 from db.database import engine
 from enum import Enum
 from typing import Optional
-from router import blog_get, blog_post, user, article, product
+from router import blog_get, blog_post, file, user, article, product
 from auth import authentication
 from db import models
 from exceptions import StoryException
+from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 app.include_router(blog_get.router)
@@ -15,6 +18,7 @@ app.include_router(user.router)
 app.include_router(article.router)
 app.include_router(product.router)
 app.include_router(authentication.router)
+app.include_router(file.router)
 
 @app.get('/')
 def index():
@@ -26,4 +30,16 @@ def story_handler(request: Request, exc: StoryException):
 
 models.Base.metadata.create_all(engine)
 
+origins = [
+    'http://localhost:3000'
+]
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = origins,
+    allow_credential = True,
+    allow_methods = ["*"],
+    allow_headers = ['*']
+)
+
+app.mount('/files', StaticFiles(directory="files"), name='files')
